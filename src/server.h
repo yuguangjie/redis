390a81b41541e85750df56e052a0ed0ace51e72c
spinlock <wnzheng@gmail.com>
2017-05-03 03:07:51 +0800

    MigrateAsync: cleanup migrateAsyncGetKeys

--- src/server.h
+++ src/server.h
@@ -1740,7 +1740,6 @@ int *zunionInterGetKeys(struct redisCommand *cmd,robj **argv, int argc, int *num
 int *evalGetKeys(struct redisCommand *cmd, robj **argv, int argc, int *numkeys);
 int *sortGetKeys(struct redisCommand *cmd, robj **argv, int argc, int *numkeys);
 int *migrateGetKeys(struct redisCommand *cmd, robj **argv, int argc, int *numkeys);
-int *migrateAsyncGetKeys(struct redisCommand *cmd, robj **argv, int argc, int *numkeys);
 
 /* Cluster */
 void clusterInit(void);
