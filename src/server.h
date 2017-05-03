34b7b21c5c30a8cd56a368fc49c319f4facb1c9d
spinlock <wnzheng@gmail.com>
2017-05-03 21:48:09 +0800

    MigrateAsync: command RESTORE-ASYNC-SELECT

--- src/server.h
+++ src/server.h
@@ -1741,7 +1741,6 @@ int *evalGetKeys(struct redisCommand *cmd, robj **argv, int argc, int *numkeys);
 int *sortGetKeys(struct redisCommand *cmd, robj **argv, int argc, int *numkeys);
 int *migrateGetKeys(struct redisCommand *cmd, robj **argv, int argc, int *numkeys);
 int *migrateAsyncGetKeys(struct redisCommand *cmd, robj **argv, int argc, int *numkeys);
-int *restoreAsyncGetKeys(struct redisCommand *cmd, robj **argv, int argc, int *numkeys);
 
 /* Cluster */
 void clusterInit(void);
@@ -1988,6 +1987,7 @@ void migrateAsyncStatusCommand(client *c);
 void migrateAsyncCancelCommand(client *c);
 void restoreAsyncCommand(client *c);
 void restoreAsyncAuthCommand(client *c);
+void restoreAsyncSelectCommand(client *c);
 void restoreAsyncAckCommand(client *c);
 
 #if defined(__GNUC__)
